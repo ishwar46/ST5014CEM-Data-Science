75b763e7a5e321ef28c12f260efbf96eec530ef4
Ishwar Chaudhary <ishwarura@gmail.com>
2023-08-16 19:26:45 +0545

    update

--- Untitled.R
+++ Untitled.R
@@ -1,8 +1,25 @@
 #install.packages("tidyverse")
 library(tidyverse)
-view(starwars)
+
+Data2019 = read.csv("/Users/macbookpro/Desktop/housepricing/housepricing-2019.csv")
+
+Data2020 = read.csv("/Users/macbookpro/Desktop/housepricing/housepricing-2020.csv")
+
+Data2021 = read.csv("/Users/macbookpro/Desktop/housepricing/housepricing-2021.csv")
+
+Data2022 = read.csv("/Users/macbookpro/Desktop/housepricing/housepricing-2022.csv")
 
 
-glimpse(starwars) #VARIABLE TYPES
+View(Data2020)
 
-data()
+#Binding Data
+
+colnames(Data2020) = c("HouseID", "Price", "Date", "Postcode", "PAON", "SAON", "FL",
+                     "HouseNum", "Flat","Street", "Locality","Town", "District", "County","Type1","Type2")
+colnames(Data2020) = c("HouseID", "Price", "Date", "Postcode", "PAON", "SAON", "FL",
+                     "HouseNum", "Flat","Street", "Locality","Town", "District", "County","Type1","Type2")
+colnames(Data2021) = c("HouseID", "Price", "Date", "Postcode", "PAON", "SAON", "FL",
+                     "HouseNum", "Flat","Street", "Locality","Town", "District", "County","Type1","Type2")
+colnames(Data2022) = c("HouseID", "Price", "Date", "Postcode", "PAON", "SAON", "FL",
+                     "HouseNum", "Flat","Street", "Locality","Town", "District", "County","Type1","Type2")
+
